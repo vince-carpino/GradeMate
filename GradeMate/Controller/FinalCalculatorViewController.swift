9d82a7b10c621e04edcfca05c51aa03fbd806a02
Vince Carpino <carpi111@mail.chapman.edu>
2018-02-01 22:51:56 -0800

    Add new branch

--- GradeMate/Controller/FinalCalculatorViewController.swift
+++ GradeMate/Controller/FinalCalculatorViewController.swift
@@ -80,11 +80,6 @@ class FinalCalculatorViewController: PickerViewController {
         setAllButtonStyles()
 
         setAllButtonTextBehavior()
-
-
-
-
-        gradeMateButton.layer.shadowRadius = 0
     }
     
     override func didReceiveMemoryWarning() {
