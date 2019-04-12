dcf3c805ce524ae799f566c241e0e6c48d0b76a6
Vince Carpino <vcarpino33@gmail.com>
2019-04-12 14:16:23 -0700

    Adds helper methods to saved classes page

--- GradeMate/SavedClasses/ClassButtonPage.swift
+++ GradeMate/SavedClasses/ClassButtonPage.swift
@@ -11,19 +11,65 @@ class ClassButtonPage: UIView {
     let USER_DEFAULTS_KEY_FOR_CURRENTLY_SELECTED_CLASS = "selectedClass"
     let USER_DEFAULTS_KEY_FOR_ALREADY_ROLLED_TO_CLASS  = "alreadyRolledToClass"
 
+    let defaultButtonColor       = UIColor.turquoise()
+    let defaultButtonShadowColor = UIColor.greenSea()
+
+    let activeButtonColor       = UIColor.concrete()
+    let activeButtonShadowColor = UIColor.asbestos()
+
     let userDefaults = UserDefaults.standard
 
-    @IBAction func buttonClicked(_ sender: FUIButton) {
+    fileprivate func setSelectedClass(_ sender: FUIButton) {
         if userDefaults.object(forKey: USER_DEFAULTS_KEY_FOR_CURRENTLY_SELECTED_CLASS) == nil {
             userDefaults.set(-1, forKey: USER_DEFAULTS_KEY_FOR_CURRENTLY_SELECTED_CLASS)
             userDefaults.set(true, forKey: USER_DEFAULTS_KEY_FOR_ALREADY_ROLLED_TO_CLASS)
         } else {
             let buttonTextToInt = convertStringWithPercentToInt(value: sender.titleLabel?.text!)
             userDefaults.set(buttonTextToInt, forKey: USER_DEFAULTS_KEY_FOR_CURRENTLY_SELECTED_CLASS)
-            userDefaults.set(false, forKey: USER_DEFAULTS_KEY_FOR_ALREADY_ROLLED_TO_CLASS)
+            userDefaults.set(sender.buttonColor == defaultButtonColor ? true : false, forKey: USER_DEFAULTS_KEY_FOR_ALREADY_ROLLED_TO_CLASS)
         }
+    }
 
-        // FIXME: Toggle button color when selected, toggling active selectedClass
+    public func resetAllOtherButtonColors(_ sender: FUIButton) {
+        let allButtons: [FUIButton] = [
+            button1,
+            button2,
+            button3,
+            button4,
+            button5
+        ]
+
+        allButtons.forEach { (button) in
+            if (sender != button) {
+                button.buttonColor = defaultButtonColor
+                button.shadowColor = defaultButtonShadowColor
+            }
+        }
+    }
+
+    public func getAllButtons() -> [FUIButton] {
+        return [
+            button1,
+            button2,
+            button3,
+            button4,
+            button5
+        ]
+    }
+
+    fileprivate func setSelectedButtonColors(_ sender: FUIButton) {
+        // FIXME: Toggle active selectedClass
+
+        sender.buttonColor = sender.buttonColor == defaultButtonColor ? activeButtonColor : defaultButtonColor
+        sender.shadowColor = sender.shadowColor == defaultButtonShadowColor ? activeButtonShadowColor : defaultButtonShadowColor
+
+        userDefaults.set(sender.buttonColor == defaultButtonColor ? true : false, forKey: USER_DEFAULTS_KEY_FOR_ALREADY_ROLLED_TO_CLASS)
+    }
+
+    @IBAction func buttonClicked(_ sender: FUIButton) {
+        resetAllOtherButtonColors(sender)
+        setSelectedButtonColors(sender)
+        setSelectedClass(sender)
     }
 
     func convertStringWithPercentToInt(value: String?) -> Int? {
